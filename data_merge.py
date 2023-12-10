37981a8272c669af776f432dcc2ac6b83f285337
alianlbj23 <alianlbj23@gmail.com>
2023-12-10 16:04:55 +0800

    first

---
+++ data_merge.py
@@ -0,0 +1,11 @@
+import pandas as pd
+
+# 加載CSV文件
+df1 = pd.read_csv('file1.csv')
+df2 = pd.read_csv('file2.csv')
+
+# 根據ID合併這兩個DataFrame
+merged_df = pd.merge(df1, df2, on='id', how='inner')
+
+# 檢查合併後的DataFrame
+print(merged_df.head())
